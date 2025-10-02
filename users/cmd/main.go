a4b5ebe7233552c4f6b3828d16e8479820986b58
sskorolev <sskorolev@avito.ru>
2025-10-02 19:37:59 +0300

    hw-2 add wire di

--- users/cmd/main.go
+++ users/cmd/main.go
@@ -3,38 +3,21 @@ package main
 import (
 	"log"
 	"net"
-
-	"users/internal/app/repository"
-	"users/internal/app/usecase"
-	pb "users/pkg/api"
-
-	deliveryGrpc "users/internal/app/delivery/grpc"
-	errorsMiddleware "users/internal/middleware/errors"
-
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
 )
 
 func main() {
-	repo := repository.NewUsersRepositoryStub()
-
-	usersUsecase := usecase.NewUsecase(repo)
-
-	controller := deliveryGrpc.NewUsersController(usersUsecase)
+	server, err := InitializeApp()
+	if err != nil {
+		log.Fatalf("failed to initialize app: %v", err)
+	}
 
 	lis, err := net.Listen("tcp", ":8082")
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
-	server := grpc.NewServer(
-		grpc.ChainUnaryInterceptor(
-			errorsMiddleware.ErrorsUnaryInterceptor(),
-		),
-	)
-	pb.RegisterUsersServiceServer(server, controller) // регистрация обработчиков
-
-	reflection.Register(server) // регистрируем дополнительные обработчики
-
 	log.Printf("server listening at %v", lis.Addr())
+	if err := server.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
